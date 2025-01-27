7f737fdf6d2570f114943aed0e9c753f936b2e18
profsibz3090 <sibandadonty@gmail.com>
2025-01-27 12:59:58 +0200

    database connected added

---
+++ src/db/database.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = "sqlite:///fastapi-practise.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
