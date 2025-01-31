fc586abb41c330f688e3e01640eef61b84f512ec
profsibz3090 <sibandadonty@gmail.com>
2025-01-31 09:49:13 +0200

    added create token and get current user auth utility functions

---
+++ src/auth/auth.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException, status, Depends
+from jose import JWTError, jwt
+from datetime import datetime, timezone, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+
+SECRET = "dcb46dee3838803b00b1c2fa22f49b13fd6ac723c3228e503d44f217e20e6429"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_TIME = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict, expire_delta: timedelta | None = None):
+    to_encode = data.copy()
+    
+    if expire_delta:
+        expires = datetime.now(timezone.utc) + expire_delta 
+    else:
+        expires = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
+    to_encode.update({"exp": expires})
+    encoded_jwt = jwt.encode(to_encode, SECRET, ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(session: Session, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+        id: int = payload.get("id")
+        if id is None: 
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
