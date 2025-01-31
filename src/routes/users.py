86edd22a125fe8383100131b4c59ae92c838f042
profsibz3090 <sibandadonty@gmail.com>
2025-01-31 11:16:33 +0200

    make user routes protected

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from src.utils import db_users
 from src.db.database import SessionDep
 from src.models import Users, UpdateUser
+from src.utils.auth import get_current_user
 
 router = APIRouter(
     prefix="/users",
@@ -13,17 +14,17 @@ def create_user(user_data: Users, session: SessionDep):
     return db_users.create_user(user_data, session)
 
 @router.get("/")
-def get_all_users(session: SessionDep):
+def get_all_users(session: SessionDep, current_user: dict = Depends(get_current_user)):
     return db_users.get_all_users(session)
 
 @router.get("/{id}")
-def get_user(id: int, session: SessionDep):
+def get_user(id: int, session: SessionDep, current_user: dict = Depends(get_current_user)):
     return db_users.get_user(id, session)
 
 @router.patch("/{id}")
-def update_user(id: int, updated_data: UpdateUser, session: SessionDep):
+def update_user(id: int, updated_data: UpdateUser, session: SessionDep, current_user: dict = Depends(get_current_user)):
     return db_users.update_user(id, updated_data, session)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id: int, session: SessionDep):
+def delete_user(id: int, session: SessionDep, current_user: dict = Depends(get_current_user)):
     return db_users.delete_user(id, session)
