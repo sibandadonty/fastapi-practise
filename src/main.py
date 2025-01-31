8991995275eac5b608bc189750f13d773a2d6143
profsibz3090 <sibandadonty@gmail.com>
2025-01-31 10:58:22 +0200

    added login endpoint

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from src.db.database import create_db_and_tables
-from src.routes import tasks, users
+from src.routes import tasks, users, auth
 
 app = FastAPI()
 
 app.include_router(tasks.router)
 app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.on_event("startup")
 def startup():
---
+++ src/routes/auth.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from src.db.database import SessionDep
+from src.models import Users
+from src.utils.passwords import verify_hashed_password
+from src.utils.auth import create_access_token
+
+router = APIRouter(
+    tags=["Authentication"]
+)
+
+@router.post("/login")
+def login_user(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depends()):
+    invalid_cred_exception = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Invalid username or password"
+    )
+    db_user = session.get(Users, id)
+    if not db_user:
+        raise invalid_cred_exception
+    if not verify_hashed_password(form_data.password, db_user.password):
+        raise invalid_cred_exception
+    
+    token = create_access_token({id: db_user.id})
+    return {"access_token": token, "token_type": "bearer"}
