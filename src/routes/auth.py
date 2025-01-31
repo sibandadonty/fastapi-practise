de8553130e0a833bbfb3fef2418b07d65d6454e1
profsibz3090 <sibandadonty@gmail.com>
2025-01-31 11:11:38 +0200

    fixed: key error for login endpoint route

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from src.db.database import SessionDep
 from src.models import Users
+from src.utils.db_users import get_user_by_username
 from src.utils.passwords import verify_hashed_password
 from src.utils.auth import create_access_token
 
@@ -15,11 +16,11 @@ def login_user(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depen
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Invalid username or password"
     )
-    db_user = session.get(Users, id)
+    db_user = get_user_by_username(form_data.username, session)
     if not db_user:
         raise invalid_cred_exception
     if not verify_hashed_password(form_data.password, db_user.password):
         raise invalid_cred_exception
     
-    token = create_access_token({id: db_user.id})
+    token = create_access_token({"id": db_user.id})
     return {"access_token": token, "token_type": "bearer"}
