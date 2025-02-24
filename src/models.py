a5edbe8f8f1c375088ef7b0a60f7edf6ed48ef5a
profsibz3090 <sibandadonty@gmail.com>
2025-02-24 14:28:35 +0200

    added db_user utility functions

--- src/models.py
+++ src/models.py
@@ -4,6 +4,12 @@ from datetime import datetime
 
 class Users(SQLModel, table=True):
     id: Optional[int] = Field(None, primary_key=True)
+    name: str
     email: str
     password: str
     created_at: datetime
+
+class UpdateUser(SQLModel):
+    email: str
+    name: str
+    
