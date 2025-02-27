2ad8a066d994450958909e88883cf48ff23d44cf
profsibz3090 <sibandadonty@gmail.com>
2025-02-27 08:46:41 +0200

    using query parameters added

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/greet/{name}")
-def root(name: str):
-    return {"message": f"hello {name}"}
+def root(name: str, age: int):
+    return {"message": f"hello {name} you are {age} years old"}
