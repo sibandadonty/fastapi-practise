133584d1000c8fa52248520fcbe3be7693fcb7de
profsibz3090 <sibandadonty@gmail.com>
2025-02-27 08:43:06 +0200

    beyond crud ground zero

--- src/main.py
+++ src/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
-from src.db.database import create_db_and_tables
-from src.routes import users, auth
 
 app = FastAPI()
 
-app.include_router(users.router)
-app.include_router(auth.router)
-
-@app.on_event("startup")
-def startup():
-    create_db_and_tables()
-
 @app.get("/")
 def root():
-    return {"message": "Hello World!!!"}
+    return {"message": "hello world"}
