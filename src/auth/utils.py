36f89a92b278e5a2583a7cd803d44bcfdedaa3de
profsibz3090 <sibandadonty@gmail.com>
2025-03-02 12:49:34 +0200

    added encoding and decoding token utility functions

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,9 @@
+import uuid
 from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+from src.config import settings
+import jwt
+import logging
 
 password_ctx = CryptContext(schemes=["bcrypt"])
 
@@ -7,3 +12,30 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return password_ctx.verify(plain_password, hashed_password)
+
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False):
+    payload = {}
+
+    payload["user"] = user_data
+    payload["exp"] = datetime.now() + expiry if expiry is not None else timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_TIME)
+    payload["jti"] = str(uuid.uuid4())
+    payload["refresh"] = refresh
+
+    access_token = jwt.encode(
+        payload=payload,
+        key=settings.JWT_SECRET,
+        algorithm=settings.JWT_ALGORITHM
+    )
+    return access_token
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            jwt=token,
+            algorithms=[settings.JWT_ALGORITHM],
+            key=settings.JWT_SECRET
+        )
+        return payload
+    except jwt.PyJWTError as e:
+        logging.exception(e)
+        return None
--- src/config.py
+++ src/config.py
@@ -5,7 +5,9 @@ DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_TIME: int
     model_config = SettingsConfigDict(env_file=DOTENV, extra="ignore")
 
 settings = Settings()
