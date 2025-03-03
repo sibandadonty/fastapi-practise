9cf54e07426928f38f2f547ac154542d2e900c60
profsibz3090 <sibandadonty@gmail.com>
2025-03-03 10:44:10 +0200

    added token data verification to token bearer class

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -2,7 +2,7 @@ from fastapi.security import HTTPBearer
 from src.auth.utils import verify_token
 from fastapi import status, HTTPException, Request
 
-class AccessTokenBearer(HTTPBearer):
+class TokenBearer(HTTPBearer):
     def __init__(self, auto_error = True):
         super().__init__(auto_error=auto_error)
     
@@ -21,9 +21,31 @@ class AccessTokenBearer(HTTPBearer):
                     "resolution": "Get a new token"
                 }
             )
+        
+        self.verify_token_data(token_data)
+
         return token_data
+    
+    def verify_token_data(self, token_data: dict):
+        raise NotImplementedError("Implement this method in child classes")
 
     async def token_valid(self, token: str):
         token = verify_token(token)
 
         return token is not None
+    
+class AccessTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict):
+        if token_data and token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token"
+            )
+        
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict):
+        if token_data and not token_data["refresh"]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide a refresh token"
+            )
