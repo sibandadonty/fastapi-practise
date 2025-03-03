5d69c299d9622ead56ad6bf496a1b6f981cbdefc
profsibz3090 <sibandadonty@gmail.com>
2025-03-03 09:23:23 +0200

    overide for the init and call httpbearer methods

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,4 +1,29 @@
 from fastapi.security import HTTPBearer
+from src.auth.utils import verify_token
+from fastapi import status, HTTPException, Request
 
 class AccessTokenBearer(HTTPBearer):
-    pass
+    def __init__(self, auto_error = True):
+        super().__init__(auto_error=auto_error)
+    
+    async def __call__(self, request: Request):
+        creds = await super().__call__(request)
+        
+        token = creds.credentials
+        
+        token_data = verify_token(token)
+        
+        if not self.token_valid(token):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={
+                    "error": "Token is expired or invalid",
+                    "resolution": "Get a new token"
+                }
+            )
+        return token_data
+
+    async def token_valid(self, token: str):
+        token = verify_token(token)
+
+        return token is not None
