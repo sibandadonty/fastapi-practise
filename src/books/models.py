e30c53e132cce6e21b93c0e014b79eeb409ea960
profsibz3090 <sibandadonty@gmail.com>
2025-03-17 09:05:02 +0200

    added user model

---
+++ src/books/models.py
@@ -0,0 +1,24 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import date, datetime
+import uuid
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            index=True,
+            nullable=False,
+            unique=True
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: date
+    page_count: int
+    language: str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: date = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
