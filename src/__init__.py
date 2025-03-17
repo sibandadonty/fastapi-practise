4d009098213a30aef6ec3f50ad084a0649a0674d
profsibz3090 <sibandadonty@gmail.com>
2025-03-17 09:33:10 +0200

    added database initialisation using lifespan events

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from src.db.main import initdb
+from contextlib import asynccontextmanager
 
 version="v1"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await initdb()
+    print("server started.......")
+    yield
+    print("server stopped.......")
 
 app = FastAPI(
     title="Bookly",
     description="A simple API for managing books",
     version=version,
+    lifespan=lifespan
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
--- src/db/main.py
+++ src/db/main.py
@@ -2,3 +2,13 @@ from sqlmodel import SQLModel, create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
 
+async_engine = AsyncEngine(
+    create_engine(
+        Config.DATABASE_URL,
+        echo=True
+    )
+)
+
+async def initdb():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
