248678822a2f45c96dedd49ddb699c910d2bfd05
profsibz3090 <sibandadonty@gmail.com>
2025-03-30 08:11:04 +0200

    added club model

---
+++ src/books/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from  sqlmodel import SQLModel, Field, Column, func
+import uuid
+import sqlalchemy.dialects.postgresql as pg
+
+class Club(SQLModel, table=True):
+    __tablename__ = "clubs"
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+          pg.UUID,
+          primary_key=True,
+          nullable=False,
+          index=True,
+          default=uuid.uuid4
+        )
+    )
+    name: str
+    location: str
+    position: int
+    ucl_trophies_number: int
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=func.now))
