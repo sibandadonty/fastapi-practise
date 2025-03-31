ce7166f83640ae64448395d7fed7b1ebf94c0c01
profsibz3090 <sibandadonty@gmail.com>
2025-03-31 10:24:08 +0200

    added get new access token endpoint

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -6,11 +6,14 @@ from src.auth.models import User
 from src.db.main import get_session
 from src.auth.utils import verify_password
 from src.auth.auth import create_access_token
-from datetime import timedelta
+from datetime import datetime, timedelta
 from fastapi.responses import JSONResponse
+from src.auth.dependencies import RefreshTokenBearer, AccessTokenBearer
 
 auth_router = APIRouter()
 auth_service = AuthService()
+refresh_token_bearer = RefreshTokenBearer()
+access_token_bearer = AccessTokenBearer()
 
 @auth_router.post("/login")
 async def login_user(login_data: LoginModel, session: AsyncSession = Depends(get_session)):
@@ -49,7 +52,7 @@ async def login_user(login_data: LoginModel, session: AsyncSession = Depends(get
               }
             )
         
-        raise HTTPException(
+    raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid email or password"
         )
@@ -58,3 +61,20 @@ async def login_user(login_data: LoginModel, session: AsyncSession = Depends(get
 async def create_user(user_data: CreateUserModel, session: AsyncSession = Depends(get_session)):
     user = await auth_service.create_user(user_data, session)
     return user
+
+@auth_router.get("/refresh_token")
+async def get_new_access_token(token_details: dict = Depends(refresh_token_bearer)):
+    expiry_timestamp = token_details['exp']
+
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(token_details['user'])
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"access_token": new_access_token}
+        )
+    
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Invalid or expired token"
+    )
