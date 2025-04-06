59b8e06197a5ecbb7616656eb7afc4e2e4535642
profsibz3090 <sibandadonty@gmail.com>
2025-04-06 11:19:37 +0200

    added init_db and get_session function to create table and get database session

--- src/db/main.py
+++ src/db/main.py
@@ -1,4 +1,24 @@
 from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 from .models import Book
+from src.config import Config
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio.session import AsyncSession
 
+engine = AsyncEngine(
+    create_engine(Config.DATABASE_URL)
+)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async_session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with async_session() as session: 
+        yield session
